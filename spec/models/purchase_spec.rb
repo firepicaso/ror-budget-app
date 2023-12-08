80f79c1456cba279d3e52d776760ceb48c6af332
Mustakim <m.m.kiron@gmail.com>
2023-12-09 00:44:36 +0600

    Add unit tests for purchase model

---
+++ spec/models/purchase_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe Purchase, type: :model do
+  let(:valid_attributes) { { 'author' => @user, 'name' => 'Electric Kettle', 'amount' => 1 } }
+  let(:no_name) { { 'author' => @user, 'amount' => 1 } }
+  let(:no_author) { { 'name' => 'Electric Kettle', 'amount' => 1 } }
+  let(:no_amount) { { 'author' => @user, 'name' => 'Electric Kettle' } }
+  let(:amount_string) do
+    { 'author' => @user, 'name' => 'Electric Kettle', 'amount' => 'two' }
+  end
+  let(:amount_float) do
+    { 'author' => @user, 'name' => 'Electric Kettle', 'amount' => 20.0 }
+  end
+  let(:amount_negative) do
+    { 'author' => @user, 'name' => 'Electric Kettle', 'amount' => -2 }
+  end
+
+  before :all do
+    User.delete_all
+    @user = User.create(name: 'dan', email: 'dan@example.com', password: 'password')
+  end
+
+  context 'user' do
+    it 'is valid when attribute exists' do
+      expect(Purchase.create(valid_attributes)).to be_valid
+    end
+
+    it 'is not valid when attribute is blank' do
+      expect(Purchase.create(no_author)).to_not be_valid
+    end
+  end
+
+  context 'name' do
+    it 'is valid when attribute exists' do
+      expect(Purchase.create(valid_attributes)).to be_valid
+    end
+
+    it 'is not valid when attribute is blank' do
+      expect(Purchase.create(no_name)).to_not be_valid
+    end
+  end
+
+  context 'amount' do
+    it 'is not valid when attribute is blank' do
+      expect(Purchase.create(no_amount)).to_not be_valid
+    end
+
+    it 'is valid when attribute is float' do
+      expect(Purchase.create(amount_float)).to be_valid
+    end
+  end
+end
