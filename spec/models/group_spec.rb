f0eb1a81b93dca68e2d4aace9f455a6df24d9b42
Mustakim <m.m.kiron@gmail.com>
2023-12-09 00:34:59 +0600

    Add unit tests for the groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,27 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  let(:valid_attributes) { { 'user' => @user, 'name' => 'Food', 'icon' => 'missing_avatar.png' } }
-  let(:no_name) { { 'user' => @user, 'icon' => 'missing_avatar.png' } }
-  let(:no_user) { { 'name' => 'Food', 'icon' => 'missing_avatar.png' } }
-  let(:name_too_long) { { 'user' => @user, 'name' => 'A' * 37, 'icon' => 'missing_avatar.png' } }
-  let(:no_icon) { { 'user' => @user, 'name' => 'Food' } }
-  let(:icon_too_long) do
-    { 'user' => @user, 'name' => 'Food', 'icon' => 'A' * 251 }
-  end
+  let(:valid_attributes) { { 'user' => @user, 'name' => 'Office', 'icon' => 'default.png' } }
+  let(:no_name) { { 'user' => @user, 'icon' => 'default.png' } }
+  let(:no_icon) { { 'user' => @user, 'name' => 'Office' } }
 
   before :all do
-    # User.delete_all
     @user = User.create(name: 'dan', email: 'dan@example.com', password: 'password')
   end
 
   context 'user' do
     it 'is valid when attribute exists' do
       expect(Group.create(valid_attributes)).to be_valid
-    end
-
-    it 'is not valid when attribute is blank' do
-      expect(Group.create(no_user)).to_not be_valid
     end
   end
 
@@ -33,15 +23,6 @@ RSpec.describe Group, type: :model do
     it 'is not valid when attribute is blank' do
       expect(Group.create(no_name)).to_not be_valid
     end
-
-    it 'is not valid when attribute length is more than 36' do
-      expect(Group.create(name_too_long)).to_not be_valid
-    end
-
-    it 'is not valid when attribute is not unique' do
-      Group.create(name: 'Food', icon: 'missing_avatar.png')
-      expect(Group.create(name: 'Food', icon: 'missing_avatar.png')).to_not be_valid
-    end
   end
 
   context 'icon' do
@@ -51,10 +32,6 @@ RSpec.describe Group, type: :model do
 
     it 'is not valid when attribute is blank' do
       expect(Group.create(no_icon)).to_not be_valid
-    end
-
-    it 'is not valid when attribute length is more than 250' do
-      expect(Group.create(icon_too_long)).to_not be_valid
     end
   end
 end
