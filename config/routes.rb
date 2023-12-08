083b4d151794c2e428bef051916af5a9cd976053
Mustakim <m.m.kiron@gmail.com>
2023-12-08 12:36:54 +0600

    Configured routes file to show splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  root "groups#index"
 
   devise_scope :user do
     authenticated :user do
