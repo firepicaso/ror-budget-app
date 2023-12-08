06604f3537e726716ad5178d992f0fa3155be323
Mustakim <m.m.kiron@gmail.com>
2023-12-08 18:14:30 +0600

    Update controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,20 +3,50 @@ class GroupsController < ApplicationController
     @groups = current_user.groups
   end
 
-  def show; end
+  def new
+    @group = Group.new
+  end
 
-  def new; end
+  def create
+    @group = current_user.groups.new(group_params)
+    if @group.save
+      flash[:success] = 'Category created successfully!'
+      redirect_to groups_path
+    else
+      flash.now[:error] = @group.errors.full_messages.to_sentence
+      render :new
+    end
+  end
 
-  def edit; end
+  def edit
+    @group = Group.find(params[:id])
+  end
 
-  def update; end
-
-  def delete; end
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:success] = 'Category updated successfully!'
+      redirect_to group_purchases_path(@group)
+    else
+      flash.now[:error] = @group.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
 
   def destroy
     @group = Group.find(params[:id])
-    @group.destroy!
-    flash[:success] = 'successfully deleted the Category'
-    redirect_to groups_url
+    if @group.destroy
+      flash[:success] = 'Category was deleted successfully!'
+      redirect_to groups_url
+    else
+      flash[:error] = @group.errors.full_messages.to_sentence
+      render :index
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
